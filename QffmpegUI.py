93e88f90d78431a9685384a5249ba00ff991f1f3
wklchris <wklchris@hotmail.com>
2020-04-04 00:45:37 -0700

    Add: File & About tabs.

---
+++ QffmpegUI.py
@@ -0,0 +1,59 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QListWidgetItem
+from Ui_QffmpegUI import Ui_winMain
+
+# High DPI support
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+class GForm(QMainWindow, Ui_winMain):
+    def __init__(self):
+        super(GForm, self).__init__()
+        self.setupUi(self)
+        # Set the file browser button
+        self.btnFileBrowser.clicked.connect(self.file_browser)
+        self.btnClearList.clicked.connect(self.clear_filelist)
+        self.btnMoveUpFile.clicked.connect(self.move_up_file)
+        self.btnDeleteFile.clicked.connect(self.delete_file)
+    
+    # --- File Tab ---
+    def file_browser(self):
+        options = QFileDialog.Options()
+        fnames, _ = QFileDialog.getOpenFileNames(
+            self, "QFileDialog.getOpenFileName()", "",
+            "All Files (*)", options=options)
+        ## Add file names to the list widget
+        if fnames:
+            for fp in fnames:
+                self.lstFileList.addItem(QListWidgetItem(f"{fp}"))
+        self.update_filenum()
+
+    def update_filenum(self):
+        n = self.lstFileList.count()
+        self.lblTotalFiles.setText(QtCore.QCoreApplication.translate(
+            "winMain", f"{n} files selected."))
+        return n
+    
+    def move_up_file(self):
+        currentRow = self.lstFileList.currentRow()
+        currentItem = self.lstFileList.takeItem(currentRow)
+        r = max(currentRow-1, 0)
+        self.lstFileList.insertItem(r, currentItem)
+
+    def delete_file(self):
+        currentRow = self.lstFileList.currentRow()
+        self.lstFileList.takeItem(currentRow)
+
+        n = self.update_filenum()
+        if n > 0:
+            self.lstFileList.setCurrentRow(max(currentRow-1, 0))
+
+    def clear_filelist(self):
+        self.lstFileList.clear()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = GForm()
+    w.show()
+    sys.exit(app.exec_())
---
+++ Ui_QffmpegUI.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'c:\Programming\CodeProjects\QffmpegUI\QffmpegUI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_winMain(object):
+    def setupUi(self, winMain):
+        winMain.setObjectName("winMain")
+        winMain.resize(413, 190)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(winMain.sizePolicy().hasHeightForWidth())
+        winMain.setSizePolicy(sizePolicy)
+        winMain.setMinimumSize(QtCore.QSize(413, 190))
+        winMain.setMaximumSize(QtCore.QSize(413, 190))
+        self.centralwidget = QtWidgets.QWidget(winMain)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(10, 10, 391, 171))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
+        self.tabWidget.setSizePolicy(sizePolicy)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tabFile = QtWidgets.QWidget()
+        self.tabFile.setObjectName("tabFile")
+        self.btnFileBrowser = QtWidgets.QPushButton(self.tabFile)
+        self.btnFileBrowser.setGeometry(QtCore.QRect(315, 7, 61, 20))
+        self.btnFileBrowser.setObjectName("btnFileBrowser")
+        self.lblTotalFiles = QtWidgets.QLabel(self.tabFile)
+        self.lblTotalFiles.setGeometry(QtCore.QRect(315, 126, 61, 16))
+        self.lblTotalFiles.setObjectName("lblTotalFiles")
+        self.lstFileList = QtWidgets.QListWidget(self.tabFile)
+        self.lstFileList.setGeometry(QtCore.QRect(10, 10, 291, 131))
+        self.lstFileList.setObjectName("lstFileList")
+        self.btnClearList = QtWidgets.QPushButton(self.tabFile)
+        self.btnClearList.setGeometry(QtCore.QRect(315, 32, 61, 17))
+        self.btnClearList.setObjectName("btnClearList")
+        self.btnMoveUpFile = QtWidgets.QPushButton(self.tabFile)
+        self.btnMoveUpFile.setGeometry(QtCore.QRect(315, 87, 61, 17))
+        self.btnMoveUpFile.setObjectName("btnMoveUpFile")
+        self.btnDeleteFile = QtWidgets.QPushButton(self.tabFile)
+        self.btnDeleteFile.setGeometry(QtCore.QRect(315, 108, 61, 17))
+        self.btnDeleteFile.setObjectName("btnDeleteFile")
+        self.tabWidget.addTab(self.tabFile, "")
+        self.tabAdvance = QtWidgets.QWidget()
+        self.tabAdvance.setObjectName("tabAdvance")
+        self.tabWidget.addTab(self.tabAdvance, "")
+        self.tabAbout = QtWidgets.QWidget()
+        self.tabAbout.setObjectName("tabAbout")
+        self.textBrowser = QtWidgets.QTextBrowser(self.tabAbout)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 10, 361, 131))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.textBrowser.sizePolicy().hasHeightForWidth())
+        self.textBrowser.setSizePolicy(sizePolicy)
+        self.textBrowser.setOpenExternalLinks(True)
+        self.textBrowser.setObjectName("textBrowser")
+        self.tabWidget.addTab(self.tabAbout, "")
+        winMain.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(winMain)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(winMain)
+
+    def retranslateUi(self, winMain):
+        _translate = QtCore.QCoreApplication.translate
+        winMain.setWindowTitle(_translate("winMain", "QffmpegUI"))
+        self.btnFileBrowser.setText(_translate("winMain", "Add Files"))
+        self.lblTotalFiles.setText(_translate("winMain", "0 files selected."))
+        self.btnClearList.setText(_translate("winMain", "Clear List"))
+        self.btnMoveUpFile.setText(_translate("winMain", "Move Up"))
+        self.btnDeleteFile.setText(_translate("winMain", "Remove"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabFile), _translate("winMain", "File"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabAdvance), _translate("winMain", "Advance"))
+        self.textBrowser.setHtml(_translate("winMain", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt; font-weight:600; color:#ff5500;\">QffmpegUI</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt; font-weight:600;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Version beta</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Update in April 2020</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">An opensource GUI tool developed under Python to deliver FFmpeg features.</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">-----</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Repo: <a href=\"https://github.com/wklchris/QffmpegUI\"><span style=\" text-decoration: underline; color:#0000ff;\">wklchris/QffmpegUI</span></a></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">License: LGPL v2.1</p></body></html>"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tabAbout), _translate("winMain", "About"))
